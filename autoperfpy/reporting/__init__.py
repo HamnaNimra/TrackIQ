c71f96a11a99a46776163f9683809e152201e768
Hamna Nimra <38963550+HamnaNimra@users.noreply.github.com>
2026-01-28 21:09:03 +0000

    Create HTML report generator module

--- autoperfpy/reporting/__init__.py
+++ autoperfpy/reporting/__init__.py
@@ -2,8 +2,10 @@
 
 from .visualizer import PerformanceVisualizer
 from .pdf_generator import PDFReportGenerator
+from .html_generator import HTMLReportGenerator
 
 __all__ = [
     "PerformanceVisualizer",
     "PDFReportGenerator",
+    "HTMLReportGenerator",
 ]
