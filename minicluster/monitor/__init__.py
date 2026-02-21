299fbcbf98feda8bd8b30cf9b37bffdc1aac7dae
Hamna Nimra <38963550+HamnaNimra@users.noreply.github.com>
2026-02-20 23:17:53 -0500

    Remove LiveDashboard from __all__ in minicluster.monitor module to streamline exports and improve module clarity.

--- minicluster/monitor/__init__.py
+++ minicluster/monitor/__init__.py
@@ -3,13 +3,10 @@
 from minicluster.monitor.health_reader import HealthReader
 from minicluster.monitor.anomaly_detector import Anomaly, AnomalyDetector
 from minicluster.monitor.health_reporter import HealthReporter
-from minicluster.monitor.live_dashboard import LiveDashboard
 
 __all__ = [
     "HealthReader",
     "Anomaly",
     "AnomalyDetector",
     "HealthReporter",
-    "LiveDashboard",
 ]
-
